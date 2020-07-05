3e7738df3f8a6bbd2cf14479202dbe5050867667
Viktor.Velikov <vvelikov1988@gmail.com>
2020-07-05 16:26:21 -0700

    user form added

--- users_app/models.py
+++ users_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    first_name = models.CharField(max_length=255))
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     age = models.IntegerField()
--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User
 
+def home(request):
+    userdata = {
+        "user_data": User.objects.all()
+    }
+    return render (request, "index.html", userdata)
+
+def results(request):
+    User.objects.create(
+        first_name=request.POST['fname'],
+        last_name=request.POST['lname'],
+        email=request.POST['email'],
+        age=request.POST['age']
+)
+    return redirect('/')
+    
 # Create your views here.
